97054a33bc83318c7bdb0d349a6f333d744da1b7
zavricardo-debug <zavricardo@gmail.com>
2026-08-26 15:13:09 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,44 +1,31 @@
 function ligar() {
 
-    document.getElementById("estado").innerText =
-        "A ligar...";
-
     fetch("https://shelly-37-eu.shelly.cloud/device/relay/control", {
-        method: "POST",
+
+        body: "channel=0&turn=on&timer=1&id=441793a5621c&auth_key=SUA_AUTH_KEY",
 
         headers: {
             "Content-Type": "application/x-www-form-urlencoded"
         },
 
-        body: "channel=0&turn=on&timer=1&id=441793a5621c&auth_key=COLOQUE_AQUI_A_SUA_AUTH_KEY"
+        method: "POST"
 
     })
-    .then(async response => {
+    .then(response => response.text())
+    .then(data => {
 
-        const resultado = await response.text();
-
-        console.log("Status:", response.status);
-        console.log("Resposta Shelly:", resultado);
-
-        if (!response.ok) {
-            throw new Error(
-                "Erro HTTP " +
-                response.status +
-                ": " +
-                resultado
-            );
-        }
+        console.log("Resposta:", data);
 
         document.getElementById("estado").innerText =
-            "🟢 Shelly ligado";
+            "Resposta: " + data;
 
     })
     .catch(error => {
 
-        console.error("ERRO:", error);
+        console.error("Erro:", error);
 
         document.getElementById("estado").innerText =
-            "❌ " + error.message;
+            "Erro: " + error;
 
     });
 
